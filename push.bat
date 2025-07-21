b520275c90ab66964394f7702587eae79b3b8d72
Igor Mauricio Carvalho Bispo <igormbispo2@gmail.com>
2025-07-20 21:46:44 -0300

    Limpeza completa do projeto - Removidos arquivos desnecessários

--- push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Script para push automático de todas as mudanças para o GitHub
-
-git add .
-git commit -m "Atualizações automáticas"
-git push
-
-echo Push automático concluído!
-pause 
